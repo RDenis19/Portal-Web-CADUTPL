7724ae6af462a8e755da81aeb96eafdbf76ddd32
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2025-07-16 01:19:53 -0500

    formateo de codigo

--- socio/urls.py
+++ socio/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'socio'
 
 urlpatterns = [
-    # URL para la página "Hazte Socio"
-    # Se accederá a ella a través de /hazte-socio/
     path("hazte-socio/", views.hazte_socio, name="hazte_socio"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def login(request):
@@ -22,15 +22,15 @@ def login(request):
     return render(request, "users/login.html", {"form": form})
 
 
-# --- VISTAS NUEVAS SOLO PARA MOSTRAR EL FRONTEND ---
-
 def change_password(request):
     # Muestra el Paso 1: Solicitar correo
     return render(request, 'users/change_password.html')
 
+
 def password_verify_code(request):
     # Muestra el Paso 2: Ingresar código
     return render(request, 'users/password_verify_code.html')
+
 
 def password_reset_confirm(request):
     # Muestra el Paso 3: Ingresar nueva contraseña
